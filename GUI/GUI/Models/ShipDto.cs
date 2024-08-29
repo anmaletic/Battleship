a937da09fdd86bc455a44129fa3ba480ea893252
AnMal <junkie_@msn.com>
2024-08-29 09:43:15 +0200

    Create simplified models of Square and Ship

---
+++ GUI/GUI/Models/ShipDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Vsite.Oom.Battleship.Model;
+
+namespace Vsite.Oom.Battleship.GUI.Models;
+
+public class SquareDto
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+    public SquareState SquareState { get; set; }
+}
+
+public class ShipDto
+{
+    public List<SquareDto> Squares { get; set; } = [];
+}
