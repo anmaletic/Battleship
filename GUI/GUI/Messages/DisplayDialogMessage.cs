01ecec4cf130ea77ecf32149fd36ee3a65c2febb
AnMal <junkie_@msn.com>
2024-08-29 09:41:43 +0200

    Implement new event messages

---
+++ GUI/GUI/Messages/DisplayDialogMessage.cs
@@ -0,0 +1,12 @@
+﻿using Vsite.Oom.Battleship.GUI.Models;
+
+namespace Vsite.Oom.Battleship.GUI.Messages;
+
+public class DisplayDialogMessage
+{
+    public  DisplayMessage Msg { get; set; }
+    public DisplayDialogMessage(DisplayMessage msg)
+    {
+        Msg = msg;
+    }
+}
